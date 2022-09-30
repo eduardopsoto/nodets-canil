17d766c63ade1019d8fa168a76628d8e0911479f
Eduardo Soto <eduardopsoto@gmail.com>
2022-09-30 11:13:01 -0400

    Incluindo as informações do model pet no controller e na view. Observação: Mudei o nome do model para pet (antes era pets).

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,43 +1,56 @@
 import {Request, Response} from "express";
 
-import { createMenuObjects } from "../helpers/createMenuObject";
+import { Pet } from '../models/pet';
+import { createMenuObjects } from '../helpers/createMenuObject';
 
 export const home = (req: Request, res: Response) => {
+    let list = Pet.getAll();
+
     res.render('pages/page', {
         menu: createMenuObjects('all'),
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
-        }
+        },
+        list
     });
 }
 
 export const dogs = (req: Request, res: Response) => {
+    let list = Pet.getFromType('dog');
+
     res.render('pages/page', {
         menu: createMenuObjects('dog'),
         banner: {
             title: 'Cachorros',
             background: 'banner_dog.jpg'
-        }
+        },
+        list
     });
 }
 
 export const cats = (req: Request, res: Response) => {
+    let list = Pet.getFromType('cat');
+
     res.render('pages/page', {
         menu: createMenuObjects('cat'),
         banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
-        }
+        },
+        list
     });
 }
 
 export const fishes = (req: Request, res: Response) => {
+    let list = Pet.getFromType('fish');
+
     res.render('pages/page', {
         menu: createMenuObjects('fish'),
         banner: {
             title: 'Peixes',
             background: 'banner_fish.jpg'
-        }
+        },
+        list
     });
 }
